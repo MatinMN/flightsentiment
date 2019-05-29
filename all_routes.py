9a25297a0080465f030aae196b6145e382641465
Govardhan Padmanabhan <39567171+GovardhanPadmanabhan@users.noreply.github.com>
2019-05-29 16:05:43 +0800

    to plot all possible routes between source and destination

---
+++ all_routes.py
@@ -0,0 +1,88 @@
+import plotly.plotly as py
+import plotly
+import plotly.graph_objs as go
+import json
+from graph import graph
+plotly.tools.set_credentials_file(username='azraf019', api_key='NY8kl4MVn6yUugcM6Gxc')
+
+list2 = graph.getAllPossiblePaths()
+
+#list2 = [[1,2,3,9,90], [1,3,7,9, 100], [1, 6, 5, 9, 1700], [1, 6, 7, 8, 9, 10000]]
+
+for i in range(len(list2)):
+    if len(list2[i]) < 6:
+        list2[i].append(None)
+        list2[i][len(list2[i])-1], list2[i][len(list2[i]) - 2] = list2[i][len(list2[i]) - 2], list2[i][len(list2[i])-1]
+        list2[i][len(list2[i])-3], list2[i][len(list2[i]) - 2] = list2[i][len(list2[i]) - 2], list2[i][len(list2[i])-3]
+
+with open('cities_with_id.json', 'r') as f:
+    cities = json.load(f)
+cities = cities['data']['cities']
+
+lats = []
+long=[]
+l_lats = []
+l_long = []
+texts=['Malaysia', 'Singapore', 'Shanghai', 'Tokyo', 'Munich', 'London', 'Ontario', 'Virginia', 'Cairo', 'Dubai']
+
+for i in range(1,11):
+    lats.append(cities[i]['lat'])
+    long.append(cities[i]['long'])
+    
+    
+airports = [go.Scattergeo(
+    lon = long,
+    lat = lat,
+    hoverinfo = 'text',
+    text = texts,
+    mode = 'markers',
+    marker = go.scattergeo.Marker(
+        size = 2,
+        color = 'rgb(255, 0, 0)',
+        line = go.scattergeo.marker.Line(
+            width = 3,
+            color = 'rgba(68, 68, 68, 0)'
+        )
+    ))]
+
+flight_paths = []
+for i in range(len(list2)):
+    lats=[]
+    long=[]
+    
+    for j in range(0, len(list2[i])-1):
+        if list2[i][j] != None:
+            lats.append(cities[list2[i][j]]['lat'])
+            long.append(cities[list2[i][j]]['long'])
+    
+    l_lats.append(lats)
+    l_long.append(long)
+    
+
+for i in range(len(list2)):
+    flight_paths.append(
+        go.Scattergeo(
+            lon = l_long[i],
+            lat = l_lats[i],
+            mode = 'lines',
+            line = go.scattergeo.Line(
+                width = 1,
+                color = 'red',
+            )
+        )
+    )
+    
+layout = go.Layout(
+    title = go.layout.Title(
+        text = 'All Possible Flight Routes'
+    ),
+    showlegend = False,
+    geo = go.layout.Geo(
+        showland = True,
+        landcolor = 'rgb(243, 243, 243)',
+        countrycolor = 'rgb(204, 204, 204)',
+    ),
+)
+
+fig = go.Figure(data = flight_paths + airports, layout = layout)
+py.plot(fig, filename = 'd3-flight-paths')
