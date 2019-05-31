90828148031975294338d7a83a9a885372d07c95
Matin <matin.mn@hotmail.com>
2019-05-31 13:45:42 +0800

    increase number of news articles per city

--- sentiment.py
+++ sentiment.py
@@ -33,7 +33,7 @@ def get_sentiment():
         results["negative"] = 0
         results["positive"] = 0
 
-        for i in range(1):
+        for i in range(4):
             url = articles[i]['url']
 
             results = get_positivity_score(name,url,results,i)
